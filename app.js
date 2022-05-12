000e94ed17548d48517c655611ebb26aeb4b6e23
vMUSTANGv <80420955+vMUSTANGv@users.noreply.github.com>
2022-05-12 08:51:47 +0300

    Feat: redesign keyboard

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// alert("Проверь пожалуйста в последний день). Спасибо)
-let line1 = {
+let line1en = {
     "Backquote": "`",
     "Digit1": "1",
     "Digit2": "2",
@@ -15,7 +14,7 @@ let line1 = {
     "Equal": "=",
     "Backspace": "Backspace"
 };
-let line2 = {
+let line2en = {
     "Tab": "Tab",
     "KeyQ": "q",
     "KeyW": "w",
@@ -31,7 +30,7 @@ let line2 = {
     "BracketRight": "]",
     "Enter": "Enter"
 };
-let line3 = {
+let line3en = {
     "CapsLock": "CapsLock",
     "KeyA": "a",
     "KeyS": "s",
@@ -46,7 +45,7 @@ let line3 = {
     "Quote": "'",
     "Backslash": "\\"
 };
-let line4 = {
+let line4en = {
     "ShiftLeft": "Shift",
     "KeyZ": "z",
     "KeyX": "x",
@@ -60,7 +59,7 @@ let line4 = {
     "Slash": "/",
     "ShiftRight": "Shift"
 };
-let line5 = {
+let line5en = {
     "ControlLeft": "Control",
     "MetaLeft": "Meta",
     "AltLeft": "Alt",
@@ -69,84 +68,88 @@ let line5 = {
     "ControlRight": "Control"
 };
 
-document.body.innerHTML = `<div class="container">
-  <textarea name="" id="" cols="103" rows="5" class="input"></textarea>
-    <div class="keyboard_lines">
-      <div class="line line1">${genDiv(line1)}</div>
-      <div class="line line2">${genDiv(line2)}</div>
-      <div class="line line3">${genDiv(line3)}</div>
-      <div class="line line4">${genDiv(line4)}</div>
-      <div class="line line5">${genDiv(line5)}</div>
-  </div>
-  </div>`;
-const KEYS = document.querySelectorAll('.key');
-const INPUT = document.querySelector('.input');
-const CAPSLOCK = document.querySelector('.CapsLock');
-CAPSLOCK.classList.remove('key');
-// const LINE2 = document.querySelector('.line2');
-// const BACKSPACE = document.querySelector('.Backspace');
-//backspace
-document.addEventListener('keydown', (e) => {
-    if (e.key == 'Backspace') {
-        let out = INPUT.textContent;
-        INPUT.innerHTML = out.slice(0, -1);
+let line1ru = {
+    "Backquote": "`",
+    "Digit1": "1",
+    "Digit2": "2",
+    "Digit3": "3",
+    "Digit4": "4",
+    "Digit5": "5",
+    "Digit6": "6",
+    "Digit7": "7",
+    "Digit8": "8",
+    "Digit9": "9",
+    "Digit0": "0",
+    "Minus": "-",
+    "Equal": "=",
+    "Backspace": "Backspace"
+};
+let line2ru = {
+    "Tab": "Tab",
+    "KeyQ": "й",
+    "KeyW": "ц",
+    "KeyE": "у",
+    "KeyR": "к",
+    "KeyT": "е",
+    "KeyY": "н",
+    "KeyU": "г",
+    "KeyI": "ш",
+    "KeyO": "щ",
+    "KeyP": "з",
+    "BracketLeft": "[",
+    "BracketRight": "]",
+    "Enter": "Enter"
+};
+let line3ru = {
+    "CapsLock": "CapsLock",
+    "KeyA": "ф",
+    "KeyS": "ы",
+    "KeyD": "в",
+    "KeyF": "а",
+    "KeyG": "п",
+    "KeyH": "р",
+    "KeyJ": "о",
+    "KeyK": "л",
+    "KeyL": "д",
+    "Semicolon": "ж",
+    "Quote": "э",
+    "Backslash": "\\"
+};
+let line4ru = {
+    "ShiftLeft": "Shift",
+    "KeyZ": "я",
+    "KeyX": "ч",
+    "KeyC": "с",
+    "KeyV": "м",
+    "KeyB": "и",
+    "KeyN": "т",
+    "KeyM": "ь",
+    "Comma": "б",
+    "Period": "ю",
+    "Slash": "/",
+    "ShiftRight": "Shift"
+};
+let line5ru = {
+    "ControlLeft": "Control",
+    "MetaLeft": "Meta",
+    "AltLeft": "Alt",
+    "Space": " ",
+    "AltRight": "Alt",
+    "ControlRight": "Control"
+};
+let lang = 0;
+
+document.addEventListener("keydown", function(zEvent) {
+    if (zEvent.ctrlKey && zEvent.altKey) {
+        console.log(lang)
+        if (lang == 0) {
+            lang += 1;
+        } else { lang -= 1 }
+        return lang;
+
     }
-})
+});
 
-//capslock KEY
-document.addEventListener('keydown', (e) => {
-    if (e.key == 'CapsLock') {
-        CAPSLOCK.classList.toggle('active')
-        for (let item in line2) {
-            return line2[item].toUpperCase()
-        }
-    } else {
-        for (let item in line2) {
-            return line2[item].toUpperCase()
-        }
-    }
-})
-
-
-
-
-
-//Backspace
-document.addEventListener('click', (e) => {
-    if (e.target.classList.contains('Backspace')) {
-        let out = INPUT.textContent;
-        INPUT.innerHTML = out.slice(0, -1);
-    }
-})
-
-
-// capslock CLICK
-document.addEventListener('click', (e) => {
-    if (e.target.classList.contains('CapsLock')) {
-        CAPSLOCK.classList.toggle('active');
-    }
-})
-
-
-document.addEventListener('mousedown', (e) => {
-
-    if (e.target.closest('.key')) {
-        e.target.classList.add('active');
-    }
-    if (e.target.textContent.length < 3) {
-        if (CAPSLOCK.classList.contains('active')) {
-            INPUT.append(e.target.textContent.toUpperCase());
-        } else { INPUT.append(e.target.textContent); }
-    }
-})
-document.addEventListener('mouseup', (e) => {
-    if (e.target.closest('.key')) { e.target.classList.remove('active') }
-})
-
-
-
-
-// let out = '';
 
 function genDiv(line) {
     let out = '';
@@ -156,24 +159,145 @@ function genDiv(line) {
     }
     return out;
 }
+if (lang == 0) {
+    document.body.innerHTML = `<div class="container">
+  <textarea name="" id="" cols="103" rows="5" class="input"></textarea>
+    <div class="keyboard_lines">
+      <div class="line line1">${genDiv(line1en)}</div>
+      <div class="line line2">${genDiv(line2en)}</div>
+      <div class="line line3">${genDiv(line3en)}</div>
+      <div class="line line4">${genDiv(line4en)}</div>
+      <div class="line line5">${genDiv(line5en)}</div>
+  </div>
+  </div>`;
+}
+if (lang == 1) {
+    document.body.innerHTML = `<div class="container">
+  <textarea name="" id="" cols="103" rows="5" class="input"></textarea>
+    <div class="keyboard_lines">
+      <div class="line line1">${genDiv(line1ru)}</div>
+      <div class="line line2">${genDiv(line2ru)}</div>
+      <div class="line line3">${genDiv(line3ru)}</div>
+      <div class="line line4">${genDiv(line4ru)}</div>
+      <div class="line line5">${genDiv(line5ru)}</div>
+  </div>
+  </div>`;
+}
 
-//print key
-document.addEventListener('keydown', (e) => {
-    for (let item of KEYS) {
-        if (e.key == item.textContent && e.key.length < 3) {
-            item.classList.add('active');
-            if (CAPSLOCK.classList.contains('active')) {
-                INPUT.append(e.key.toUpperCase());
-            } else { INPUT.append(e.key); }
 
+const keys = document.querySelectorAll('.key');
+const input = document.querySelector('.input');
+// let shift = false;
+let capslock = false;
+// let ctrl = false;
+
+
+for (let item of keys) {
+    if (item.textContent.length < 3) {
+        item.classList.add('alpha');
+    }
+}
+let alphab = document.querySelectorAll('.alpha');
+
+
+
+function capsL() {
+    if (capslock == false) {
+        for (let i = 0; i < alphab.length; i++) {
+            let letter = alphab[i].textContent;
+            const upper = letter.toUpperCase();
+            alphab[i].textContent = upper;
+        }
+        capslock = true;
+    } else {
+        for (let i = 0; i < alphab.length; i++) {
+            let letter = alphab[i].textContent;
+            const lower = letter.toLowerCase();
+            alphab[i].textContent = lower;
+        }
+        capslock = false;
+    }
+}
+document.addEventListener('mousedown', (e) => {
+    if (e.target.classList.contains('CapsLock')) {
+        e.target.classList.toggle('active');
+    } else if (e.target.classList.contains('key')) {
+        e.target.classList.add('active');
+    }
+})
+
+document.addEventListener('mouseup', (e) => {
+
+    if (e.target.classList.contains('key') && !e.target.classList.contains('CapsLock')) {
+        e.target.classList.remove('active');
+    }
+})
+
+
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('key')) {
+        if (e.target.classList.contains('Space')) {
+            input.textContent += ' ';
+        }
+        if (e.target.classList.contains('CapsLock')) {
+            capsL();
+        }
+        if (e.target.classList.contains('alpha')) {
+            const getContent = e.target.textContent;
+            input.textContent += getContent;
+        }
+        if (e.target.classList.contains('Backspace')) {
+            input.textContent = input.textContent.substring(
+                0, input.textContent.length - 1
+            );
+        } else {
+            return;
         }
     }
 })
+
+document.addEventListener('keydown', (e) => {
+    for (let i = 0; i < keys.length; i++) {
+        // console.log(e.key)
+
+        if (e.key == keys[i].textContent) {
+
+            if (e.key == ' ') {
+                input.textContent += ' ';
+            }
+            if (e.key == 'CapsLock') {
+                capsL();
+            }
+            if (e.key == keys[i].textContent && e.key.length < 3) {
+                const getContent = e.key;
+                input.textContent += getContent;
+            }
+            if (e.key == 'Backspace') {
+                input.textContent = input.textContent.substring(
+                    0, input.textContent.length - 1
+                );
+            } else {
+                return;
+            }
+        }
+    }
+
+})
+
+document.addEventListener('keydown', (e) => {
+    for (let i = 0; i < keys.length; i++) {
+        const element = keys[i];
+        if (e.key == element.textContent) {
+            element.classList.add('active')
+        }
+
+    }
+})
 document.addEventListener('keyup', (e) => {
-    console.log(e.key)
-    for (let item of KEYS) {
-        if (e.key == item.textContent && e.key.length < 3) {
-            item.classList.remove('active');
+    for (let i = 0; i < keys.length; i++) {
+        const element = keys[i];
+        if (e.key == element.textContent) {
+            element.classList.remove('active')
         }
     }
 })
